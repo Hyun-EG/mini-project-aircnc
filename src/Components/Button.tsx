e0362f73b4fd02fc89c9c8b1c43357397b579299
Heejun Byeon <hejuby@gmail.com>
2024-06-18 12:20:21 +0900

    feat: 임시 스타일로 버튼 컴포넌트 추가(스타일드 컴포넌트)

---
+++ src/Components/Button.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 100%;
+  padding: 16px 0;
+  border-radius: 16px;
+  color: #fff;
+  background-color: #ff385c;
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export default Button;
