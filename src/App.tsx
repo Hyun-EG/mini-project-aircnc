bb4df3b12b06234ee62425d42be7e0ad15dd165c
SangHwa <kimsh9026@gmail.com>
2024-06-18 12:54:48 +0900

    refactor: CardList 표시 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import { useState, useEffect } from 'react';
-import CardView from './Components/CardView.tsx';
-import { MockData } from './assets/interfaces.ts';
+import CardListingPage from './Pages/CardListingPage.tsx';
 
 function App() {
-  const [listings, setListings] = useState<MockData[]>([]);
-
-  useEffect(() => {
-    const fetchListings = async () => {
-      try {
-        const response = await fetch('/src/assets/mockdata.json');
-        const data = await response.json();
-        setListings(data);
-      } catch (error) {
-        console.error('Error fetching listings:', error);
-      }
-    };
-
-    fetchListings();
-  }, []);
-
-  return (
-    <>
-      {listings.map((listing) => (
-        <CardView
-          key={listing.name + listing.address}
-          image={listing.photo}
-          name={listing.name}
-          address={listing.address}
-          info={`${listing.rooms}개의 방을 예약할 수 있습니다`}
-        />
-      ))}
-    </>
-  );
+  return <CardListingPage />;
 }
 
 export default App;
