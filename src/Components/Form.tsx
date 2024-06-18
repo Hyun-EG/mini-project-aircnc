48825cf0d59967ba8f062b46c0e4cb10ee9d6946
Heejun Byeon <hejuby@gmail.com>
2024-06-18 15:10:49 +0900

    feat: 임시 스타일로 공통 Form 컴포넌트 추가

---
+++ src/Components/Form.tsx
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+import styled from 'styled-components';
+
+const FormLayout = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  & :last-child {
+    margin-top: 32px;
+  }
+`;
+
+interface FormProps {
+  children: React.ReactNode;
+  onSubmit: () => void;
+}
+
+function Form({ children, onSubmit }: FormProps) {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  return (
+    <FormLayout ref={formRef} onSubmit={onSubmit}>
+      {children}
+    </FormLayout>
+  );
+}
+
+export default Form;
