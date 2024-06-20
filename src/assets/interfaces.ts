bc73097de9f6918b153bfe3efe8c1c8c474fd138
SangHwa <kimsh9026@gmail.com>
2024-06-20 15:23:51 +0900

    add: mockdata, interfaces

--- src/assets/interfaces.ts
+++ src/assets/interfaces.ts
@@ -4,3 +4,25 @@ export interface MockData {
   address: string;
   rooms: string;
 }
+
+export interface RoomData {
+  id: number;
+  name: string;
+  price: number;
+  address: string;
+  imageUrl: string;
+}
+
+export interface RoomDetailData {
+  id: number;
+  name: string;
+  price: number;
+  max_capacity: number;
+  description: string;
+  city: string;
+  address: string;
+  map_x: number;
+  map_y: number;
+  created_at: string;
+  imageUrl: string;
+}
