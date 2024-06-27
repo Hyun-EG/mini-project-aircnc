25e2e23cb1bb5d0e1622b544a4fa7c4030f68617
hyun-EG <codiee@naver.com>
2024-06-28 02:25:13 +0900

    style: 캘린더 스타일 커스텀

--- src/Components/DetailCalendar.tsx
+++ src/Components/DetailCalendar.tsx
@@ -1,57 +1,80 @@
+import React from 'react';
 import styled from 'styled-components';
-import { useSelector } from 'react-redux';
-import Calendar from 'react-calendar';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../redux/store.ts';
+import CalendarComponent from './Header/Calendar.tsx';
+import {
+  setCheckInDate,
+  setCheckOutDate,
+} from '../redux/slices/searchSlice.ts';
 
 const CardContainer = styled.div`
   width: 80%;
-  padding: 10px;
+  height: 50vh;
+  margin-left: 3vh;
   border-radius: 10px;
   overflow: hidden;
+  @media (max-width: 768px) {
+    width: 60%;
+  }
 `;
+
 const CalendarContainer = styled.div`
   width: 100%;
-  height: 0;
-  padding: 10px;
-  padding-bottom: 100%;
-  border-radius: 10px;
-  background-color: #e9e9e9;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 400px;
   overflow: hidden;
   position: relative;
 `;
 
-const TextContainer = styled.div`
-  margin: 10px 10px;
-`;
-
-const Address = styled.p`
-  margin: 10px 0;
-  font-size: 15px;
-  color: #666;
-`;
-const Info = styled.p`
-  margin: 10px 0;
-  font-size: 15px;
-  font-weight: bold;
-  color: #333;
+const CustomCalendar = styled(CalendarComponent)`
+  padding: 3vh;
+  border: 1px solid lightgrey;
+  position: static;
+  top: auto;
+  visibility: visible;
 `;
 
 function DetailCalendar() {
+  const dispatch = useDispatch();
   const selectedRoom = useSelector(
     (state: RootState) => state.rooms.selectedRoom,
   );
+  const checkInDate = useSelector(
+    (state: RootState) => state.search.checkInDate,
+  );
+  const checkOutDate = useSelector(
+    (state: RootState) => state.search.checkOutDate,
+  );
+
   if (!selectedRoom) {
-    return <h1>Loading</h1>;
+    return <h1>Loading...</h1>;
   }
-  // image_url이 카멜케이스가 아니라고 난리네,,ㅠㅠ 백엔드 보이..
+
+  const handleDateChange = (date: Date) => {
+    if (!checkInDate || (checkInDate && checkOutDate)) {
+      dispatch(setCheckInDate(date));
+      dispatch(setCheckOutDate(null));
+    } else if (date > checkInDate) {
+      dispatch(setCheckOutDate(date));
+    } else {
+      dispatch(setCheckInDate(date));
+      dispatch(setCheckOutDate(null));
+    }
+  };
+
   return (
     <CardContainer>
-      <CalendarContainer>calendar들어갈 자리입니다~</CalendarContainer>
-      {/* 캘린더에 클릭한 정보(검색한 정보가 있으면 표시) */}
-      <TextContainer>
-        <Info>{selectedRoom.description}</Info>
-        <Address>{selectedRoom.address}</Address>
-      </TextContainer>
+      <CalendarContainer>
+        <CustomCalendar
+          isOpen={true}
+          onDateChange={handleDateChange}
+          checkInDate={checkInDate}
+          checkOutDate={checkOutDate}
+        />
+      </CalendarContainer>
     </CardContainer>
   );
 }
