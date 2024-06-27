7c72430d0961ff92a43b92d4c6389eb530bff1e8
hyun-EG <codiee@naver.com>
2024-06-28 02:27:34 +0900

    refactor: 디테일 페이지 구현

--- src/Pages/DetailInfoPage.tsx
+++ src/Pages/DetailInfoPage.tsx
@@ -1,55 +1,119 @@
-// 숙박업소 조회 페이지
-// 헤더
-// 바디[숙소 정보 / 캘린더 + 지도]
-// 숙소 정보(숙소명 + 사진 + 정보 (카드 가져와서 붙일까..? 귀찮은데,,?ㅠ) / 캘린더 + 클릭하면 체크인/아웃 날짜 체크하고 예상 숙박 금액 표시)
-// 푸터..? 예약하기 버튼, 클릭했을 때 유효성 검사 한번 더 해서 불가능하면 빨간 글씨로 바꾸기
-import { useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { RootState } from '../redux/store.ts';
 import Header from '../Components/Header/Header.tsx';
 import DetailCard from '../Components/DetailCard.tsx';
 import DetailCalendar from '../Components/DetailCalendar.tsx';
 import DetailFooter from '../Components/DetailFooter.tsx';
-// 헤더 아래로 바디 두려고,,
+import DetailMap from '../Components/Map/DetailMap.tsx';
+import { setGuestCount } from '../redux/slices/searchSlice.ts';
+
 const BodyContainer = styled.div`
-  margin-top: 13vh;
+  margin: 13vh 0 13vh 0;
   padding: 20px;
 `;
 
-// 숙소정보 + 캘린더 넣자
 const InfoContainer = styled.div`
   width: 100%;
   overflow: hidden;
   display: flex;
+
+  @media (max-width: 1300px) {
+    flex-direction: column;
+  }
+`;
+
+const RoomBookingDetails = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const BookingDetailsContent = styled.div`
+  width: 100%;
+  font-size: 2.5vh;
+  font-weight: bold;
+  padding: 10px;
+`;
+
+const AddGuestBtn = styled.button`
+  width: 4vh;
+  height: 4vh;
+  margin-left: 2vh;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  font-size: 3vh;
+  font-weight: bold;
+  color: #fff;
+  background: #ff385c;
+`;
+
+const RoomTitle = styled.div`
+  font-size: 3vh;
+  margin-left: 2.5vh;
+`;
+
+const MapTitle = styled.div`
+  font-size: 3vh;
+  margin-bottom: 2.5vh;
 `;
 
 const MapContainer = styled.div`
-  width: 100%;
+  width: 80%;
+  margin: 0 10%;
   border-radius: 10px;
   overflow: hidden;
+  text-align: center;
+  font-size: 3vh;
+  font-weight: bold;
+`;
+
+const SeparationLine = styled.div`
+  width: 100%;
+  height: 0.1px;
+  margin: 6vh 0 5vh 0;
+  background-color: lightgrey;
 `;
 
 function DetailInfoPage() {
+  const dispatch = useDispatch();
   const selectedRoom = useSelector(
     (state: RootState) => state.rooms.selectedRoom,
   );
+  const guestCount = useSelector((state: RootState) => state.search.guestCount);
 
   if (!selectedRoom) {
-    return <h1>Loading</h1>;
+    return <h1>Loading...</h1>;
   }
+
+  const incrementGuestCount = () => {
+    dispatch(setGuestCount(guestCount + 1));
+  };
 
   return (
     <>
       <Header />
       <BodyContainer>
-        <h1>{selectedRoom.name}</h1>
+        <RoomTitle>{selectedRoom.name}</RoomTitle>
         <InfoContainer>
           <DetailCard />
-          <DetailCalendar />
+          <RoomBookingDetails>
+            <DetailCalendar />
+            <BookingDetailsContent>
+              {`Price: ${selectedRoom.price}`}
+            </BookingDetailsContent>
+            <BookingDetailsContent>
+              {`Personnel: ${guestCount}`}
+              <AddGuestBtn onClick={incrementGuestCount}>+</AddGuestBtn>
+            </BookingDetailsContent>
+          </RoomBookingDetails>
         </InfoContainer>
+        <SeparationLine />
         <MapContainer>
-          {/* 지도 넣기 */}
-          숙소 위치
+          <MapTitle>숙소 위치</MapTitle>
+          <DetailMap width="100%" height="50vh" listing={selectedRoom} />
         </MapContainer>
       </BodyContainer>
       <DetailFooter />
