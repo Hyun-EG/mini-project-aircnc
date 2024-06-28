4b35ddcadea7b55361db33004eebb6ba40ae6c88
Heejun Byeon <hejuby@gmail.com>
2024-06-28 13:55:18 +0900

    feat: Pretendard 폰트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from 'styled-components';
 import AppRoutes from './Routes/AppRoutes.tsx';
 import GlobalStyle from './style/GlobalStyle.tsx';
+import GlobalFont from './style/GlobalFont.tsx';
 import theme from './style/theme.ts';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <GlobalFont />
       <AppRoutes />
     </ThemeProvider>
   );
