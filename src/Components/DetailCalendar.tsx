6b2f994391626183067178708b0f006589aff00b
SangHwa <kimsh9026@gmail.com>
2024-07-01 00:49:45 +0900

    feat: 날짜 유효성검사

--- src/Components/DetailCalendar.tsx
+++ src/Components/DetailCalendar.tsx
@@ -7,6 +7,7 @@ import {
   setCheckInDate,
   setCheckOutDate,
 } from '../redux/slices/searchSlice.ts';
+import isDateConflict from '../util/dateUtils.ts';
 
 const CardContainer = styled.div`
   width: 100%;
@@ -58,7 +59,20 @@ function DetailCalendar() {
       dispatch(setCheckInDate(date));
       dispatch(setCheckOutDate(null));
     } else if (date > checkInDate) {
-      dispatch(setCheckOutDate(date));
+      const reservations = JSON.parse(
+        localStorage.getItem('reservedRoom') || '[]',
+      );
+      const isConflict = isDateConflict(
+        checkInDate,
+        date,
+        reservations, // 현재까지 예약된 방 정보
+        selectedRoom.id,
+      );
+      if (isConflict) {
+        alert('이미 예약된 날자가 포함되어 있습니다.');
+      } else {
+        dispatch(setCheckOutDate(date));
+      }
     } else {
       dispatch(setCheckInDate(date));
       dispatch(setCheckOutDate(null));
@@ -69,7 +83,7 @@ function DetailCalendar() {
     <CardContainer>
       <CalendarContainer>
         <CustomCalendar
-          isOpen={true}
+          isOpen
           onDateChange={handleDateChange}
           checkInDate={checkInDate}
           checkOutDate={checkOutDate}
