3e182814d0e07104e53e2bd9b32f8143026d412c
hyun-EG <codiee@naver.com>
2024-06-30 20:56:59 +0900

    style: 스크롤 바 커스텀마이징

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -3,4 +3,18 @@ import reset from 'styled-reset';
 
 export default createGlobalStyle`
   ${reset}  
+
+  ::-webkit-scrollbar {
+    width: 12px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: transparent;
+  }
+  
+  ::-webkit-scrollbar-thumb {
+    background-color: #ff385c;
+    border-radius: 6px;
+    border: 3px solid #f1f1f1;
+  }
 `;
