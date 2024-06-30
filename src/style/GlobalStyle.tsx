d4823acb4884b04cd3713bda04470017c38b1689
hyun-EG <codiee@naver.com>
2024-06-30 21:06:49 +0900

    docs: 문서수정

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -4,6 +4,10 @@ import reset from 'styled-reset';
 export default createGlobalStyle`
   ${reset}  
 
+  body {
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+  }
+
   ::-webkit-scrollbar {
     width: 12px;
   }
@@ -16,9 +20,5 @@ export default createGlobalStyle`
     background-color: #ff385c;
     border-radius: 6px;
     border: 3px solid #f1f1f1;
-  }
-  
-  body {
-    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
 `;
