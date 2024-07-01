4458bb923ee001d17572f9baf51779977caf1ef6
Heejun Byeon <hejuby@gmail.com>
2024-07-01 17:07:30 +0900

    fix: 글로벌 스타일 리셋 보완

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -8,6 +8,15 @@ export default createGlobalStyle`
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
 
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    font-family: inherit;
+  }
+
   ::-webkit-scrollbar {
     width: 12px;
   }
