5e4fc0099a60a452b5fc9133cda0e73722707bb4
Heejun Byeon <hejuby@gmail.com>
2024-07-01 16:47:58 +0900

    feat: 세부사항 지도 스타일 추가

--- src/Components/Map/DetailMap.tsx
+++ src/Components/Map/DetailMap.tsx
@@ -23,46 +23,20 @@ export default function DetailMap({ width, height, listing }: DetailMapProps) {
 
     const mapOptions = {
       center: new naver.maps.LatLng(listing.map_y, listing.map_x),
-      zoom: 18,
-      zoomControl: true,
-      zoomControlOptions: {
-        style: naver.maps.ZoomControlStyle.SMALL,
-        position: naver.maps.Position.TOP_RIGHT,
-      },
+      zoom: 16,
+      disableDoubleClickZoom: true,
+      disableDoubleTapZoom: true,
+      disableKineticPan: true,
+      disableTwoFingerTapZoom: true,
+      draggable: false,
+      pinchZoom: false,
+      scrollWheel: false,
     };
 
     const map = new naver.maps.Map('detailMap', mapOptions);
     const marker = new naver.maps.Marker({
       position: mapOptions.center,
       map,
-    });
-
-    const contentString = `
-      <div>
-        <img src="${listing.image_url}" alt="${listing.name}" style="width: 100%; height: 20vh;" />
-        <p style="font-size: 2vh; font-weight: bold;">${listing.name}</p>
-        <p style="font-size: 1.5vh;">주소: ${listing.address}</p>
-        <p style="font-size: 1.5vh;">가격: ${listing.price}원</p>
-        <p style="font-size: 1.5vh;">옵션: ${listing.description}</p>
-      </div>
-    `;
-
-    const infowindow = new naver.maps.InfoWindow({
-      content: contentString,
-      maxWidth: 250,
-    });
-
-    let openInfoWindow: naver.maps.InfoWindow | undefined;
-
-    naver.maps.Event.addListener(marker, 'mouseover', () => {
-      infowindow.open(map, marker);
-      openInfoWindow = infowindow;
-    });
-
-    naver.maps.Event.addListener(marker, 'mouseout', () => {
-      if (openInfoWindow) {
-        openInfoWindow.close();
-      }
     });
   }, [listing]);
 
