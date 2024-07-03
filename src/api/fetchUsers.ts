2dd5a8204cdce9b3ef09e83b7484dcfe329c8d9d
Heejun Byeon <hejuby@gmail.com>
2024-07-04 01:29:03 +0900

    refactor: 공통 fetchUser 함수 분리

--- src/api/fetchUsers.ts
+++ src/api/fetchUsers.ts
@@ -1,21 +1,21 @@
 import {
   EmailFormSchemaType,
   LoginFormSchemaType,
+  User,
 } from '../schema/userSchema.ts';
 
 const baseUrl = 'http://54.180.158.55:8080/api/auth';
 
+type FetchMethod = 'GET' | 'POST';
+
 interface ResponseResult {
-  resultCode: 200 | 4002;
+  resultCode: 200 | 4000 | 4001 | 4002 | 4003 | 4004;
   resultMessage: string;
 }
 
-const getIsSuccessfulFromResponse = async (
-  response: Response,
-): Promise<boolean> => {
+const isResponseSuccessful = async (response: Response) => {
   const body = await response.json();
 
-  console.log(body);
   const result: ResponseResult = {
     resultCode: body.result.result_code,
     resultMessage: body.result.result_message,
@@ -23,49 +23,60 @@ const getIsSuccessfulFromResponse = async (
   return result.resultCode === 200;
 };
 
-export const getValidateEmail = async (email: EmailFormSchemaType['email']) => {
-  const response = await fetch(`${baseUrl}/email/${email}`, {
-    method: 'GET',
+interface FetchUserArgs<T> {
+  url: string;
+  method: FetchMethod;
+  body?: T | null;
+  setToken?: boolean;
+}
+
+const fetchUser = async <T>({
+  url,
+  method,
+  body = null,
+  setToken = false,
+}: FetchUserArgs<T>): Promise<boolean> => {
+  const response = await fetch(url, {
+    method,
     headers: {
       'Content-Type': 'application/json',
       Origin: 'http://localhost:5173/',
     },
-    body: null,
+    body: body ? JSON.stringify(body) : null,
   });
 
   if (!response) {
     throw new Error();
   }
 
-  const isSuccessful = await getIsSuccessfulFromResponse(response);
+  if (setToken) {
+    const token = response.headers.get('authorization');
+    if (token) {
+      localStorage.setItem('token', token);
+    }
+  }
+
+  const isSuccessful = await isResponseSuccessful(response);
 
   return isSuccessful;
 };
+
+export const getValidateEmail = async (email: EmailFormSchemaType['email']) =>
+  await fetchUser({ url: `${baseUrl}/email/${email}`, method: 'GET' });
 
 export const postLogin = async (
   user: EmailFormSchemaType & LoginFormSchemaType,
-) => {
-  const response = await fetch(`${baseUrl}/login`, {
+) =>
+  await fetchUser<EmailFormSchemaType & LoginFormSchemaType>({
+    url: `${baseUrl}/login`,
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Origin: 'http://localhost:5173/',
-    },
-    body: JSON.stringify(user),
+    body: user,
+    setToken: true,
   });
 
-  if (!response) {
-    throw new Error();
-  }
-
-  const token = response.headers.get('authorization');
-  if (token) {
-    localStorage.setItem('token', token);
-  }
-
-  const isSuccessful = await getIsSuccessfulFromResponse(response);
-
-  return isSuccessful;
-};
-
-export const signUp = async () => {};
+export const postSignUp = async (user: User) =>
+  await fetchUser<User>({
+    url: `${baseUrl}/signup`,
+    method: 'POST',
+    body: user,
+  });
