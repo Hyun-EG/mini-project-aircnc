73d68ccc4e1c427a36c4ebdceb1d5fa552068bef
Heejun Byeon <hejuby@gmail.com>
2024-07-04 21:46:06 +0900

    feat: axios 설치 및 api 인스턴스 추가

---
+++ src/api/api.ts
@@ -0,0 +1,77 @@
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
+
+const BASE_URL = ' http://52.79.187.32:8080/api';
+
+const logOnDev = (message: string) => {
+  if (process.env.NODE_ENV === 'development') {
+    console.log(message);
+  }
+};
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+const onRequest = (config: InternalAxiosRequestConfig) => {
+  const { method, url } = config;
+
+  const token = localStorage.getItem('token');
+
+  config.headers.Authorization = token;
+
+  logOnDev(`[API] ${method?.toUpperCase()} ${url} | Request`);
+
+  return Promise.resolve(config);
+};
+
+const onResponse = (response: AxiosResponse) => {
+  const { config, status } = response;
+
+  logOnDev(
+    `[API] ${config.method?.toUpperCase()} ${config.url} | Response ${status}`,
+  );
+
+  const token = response.headers.authorization;
+
+  if (token) {
+    localStorage.setItem('token', token);
+  }
+
+  return response.data;
+};
+
+const onError = (error: AxiosError | Error) => {
+  if (axios.isAxiosError(error) && error.response) {
+    const { message } = error;
+    const { method, url } = error.config as AxiosRequestConfig;
+    const { status, statusText } = error.response as AxiosResponse;
+
+    logOnDev(
+      `[API] ${method?.toUpperCase()} ${url} | Error ${status} ${statusText} | ${message}\n`,
+    );
+    logOnDev(
+      `${error.response.data.result.result_code} | ${error.response.data.result.result_message}`,
+    );
+
+    return error.response.data;
+  }
+
+  throw new Error();
+};
+
+const setUpInterceptors = (instance: AxiosInstance): AxiosInstance => {
+  instance.interceptors.request.use(onRequest);
+  instance.interceptors.response.use(onResponse, onError);
+
+  return instance;
+};
+
+setUpInterceptors(api);
+
+export default api;
