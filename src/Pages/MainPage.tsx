f6cd12f2ef975e5adae1bf804d788bd7e7383b4f
Heejun Byeon <hejuby@gmail.com>
2024-07-04 21:46:06 +0900

    feat: axios 설치 및 api 인스턴스 추가

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -10,10 +10,6 @@ function MainPage() {
   const [listings, setListings] = useState<RoomDetailData[]>([]);
 
   useEffect(() => {
-    // alert(
-    //   '백엔드 작업이 너무 느려서 API를 연결하지 못했습니다. \n완성을 도대체 언제 하려고 그러는걸까요..? \n호기롭게 기능은 다 만들었지만 고도화중이라고 했는데;;; 너무 고도화하네..',
-    // );
-
     // Fetch current position and address
     const fetchAddress = async () => {
       try {
