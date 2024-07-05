e68aef41dc2ef806808628ac8aea4e67fb2786c1
Heejun Byeon <hejuby@gmail.com>
2024-07-05 14:52:01 +0900

    fix: 서치 페이지 깃 꼬인거 수정 및 mode 변수 조건문 수정

--- src/Pages/SearchResultPage.tsx
+++ src/Pages/SearchResultPage.tsx
@@ -27,38 +27,71 @@ const MapMargin = styled.div`
 `;
 
 function SearchResultPage() {
-  const { location } = useSelector((state: RootState) => state.search);
+  const { location, checkInDate, checkOutDate, guestCount, mode, coordinates } =
+    useSelector((state: RootState) => state.search);
   const [listings, setListings] = useState<RoomResponse[]>([]);
-  const [visibleCount, setVisibleCount] = useState(10);
   const loader = useRef<HTMLDivElement | null>(null);
+  const cursorId = useRef<number | null>(null);
 
-  useEffect(() => {
-    const fetchListings = async () => {
-      try {
-        const response = await fetch('src/assets/room_data.json');
-        const rooms: RoomResponse[] = await response.json();
-        const filteredRooms = rooms.filter((room) =>
-          room.city.includes(location),
-        );
-        setListings(filteredRooms);
-      } catch (error) {
-        console.error('Error fetching listings:', error);
-      }
-    };
-
-    if (location) {
-      fetchListings();
+  const formatDate = (isoDate: string | null) => {
+    if (isoDate === null) {
+      return;
     }
-  }, [location]);
+    const date = new Date(isoDate);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+
+  const fetchMoreListings = useCallback(async () => {
+    try {
+      let url;
+      if (mode === 'city') {
+        // http://ec2-52-79-187-32.ap-northeast-2.compute.amazonaws.com/
+        // http://52.79.187.32:8080/
+        url = `http://ec2-52-79-187-32.ap-northeast-2.compute.amazonaws.com/api/rooms/city?capacity=${guestCount}&check_in=${formatDate(checkInDate)}&check_out=${formatDate(checkOutDate)}&city=${location}`;
+      } else {
+        url = `http://52.79.187.32:8080/api/rooms/map?capacity=${guestCount}&check_in=${formatDate(checkInDate)}&check_out=${formatDate(checkOutDate)}&top=${coordinates.top}&bottom=${coordinates.bottom}&right=${coordinates.right}&left=${coordinates.left}`;
+      }
+
+      if (cursorId.current) {
+        url += `&cursor_id=${cursorId.current}`;
+      }
+
+      console.log(url);
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch room data');
+      }
+
+      const data = await response.json();
+      const roomData = data.body.room_response_list;
+
+      setListings((prevListings) => [...prevListings, ...roomData]);
+
+      if (roomData.length > 0) {
+        cursorId.current = roomData[roomData.length - 1].room_id;
+      }
+    } catch (error) {
+      console.error('Error fetching listings:', error);
+    }
+  }, [checkInDate, checkOutDate, guestCount, location, mode, coordinates]);
 
   const handleObserver = useCallback(
-    (entities: IntersectionObserverEntry[]) => {
-      const target = entities[0];
-      if (target.isIntersecting) {
-        setVisibleCount((prev) => (prev < listings.length ? prev + 10 : prev));
+    (entries: IntersectionObserverEntry[]) => {
+      const target = entries[0];
+      if (target.isIntersecting && listings.length > 0) {
+        fetchMoreListings();
       }
     },
-    [listings],
+    [fetchMoreListings, listings.length],
   );
 
   useEffect(() => {
@@ -74,18 +107,56 @@ function SearchResultPage() {
     };
   }, [handleObserver]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let url;
+        if (mode === 'city') {
+          url = `http://ec2-52-79-187-32.ap-northeast-2.compute.amazonaws.com/api/rooms/city?capacity=${guestCount}&check_in=${formatDate(checkInDate)}&check_out=${formatDate(checkOutDate)}&city=${location}`;
+        } else {
+          url = `http://ec2-52-79-187-32.ap-northeast-2.compute.amazonaws.com/api/rooms/map?capacity=${guestCount}&check_in=${formatDate(checkInDate)}&check_out=${formatDate(checkOutDate)}&top=${coordinates.top}&bottom=${coordinates.bottom}&right=${coordinates.right}&left=${coordinates.left}`;
+        }
+
+        if (cursorId.current) {
+          url += `&cursor_id=${cursorId.current}`;
+        }
+
+        console.log(url);
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch room data');
+        }
+
+        const data = await response.json();
+        const roomData = data.body.room_response_list;
+
+        setListings(roomData);
+
+        if (roomData.length > 0) {
+          cursorId.current = roomData[roomData.length - 1].room_id + 1;
+        }
+      } catch (error) {
+        console.error('Error fetching listings:', error);
+      }
+    };
+
+    fetchData();
+  }, [checkInDate, checkOutDate, guestCount, location, mode, coordinates]);
+
   return (
     <SearchPageContainer>
       <CardGridContainer>
-        <CardGrid listings={listings.slice(0, visibleCount)} />
+        <CardGrid listings={listings} />
         <CardGridWatcher ref={loader} />
       </CardGridContainer>
       <MapMargin />
-      <Map
-        width="39.5%"
-        height="95vh"
-        listings={listings.slice(0, visibleCount)}
-      />
+      <Map width="39.5%" height="95vh" listings={listings} />
     </SearchPageContainer>
   );
 }
