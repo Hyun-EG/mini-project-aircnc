da4c357150e6a86fb8e88ef4cb28691557b7cb98
hyun-EG <codiee@naver.com>
2024-07-09 03:21:21 +0900

    add: 메인페이지 스타일 추가 및 배너 이미지 파일 추가

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,15 +1,111 @@
-import { useMemo } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import styled, { keyframes } from 'styled-components';
 import CardGrid from '../Components/CardGrid.tsx';
 import { RoomResponse } from '../assets/interfaces.ts';
 import useGeolocation from '../util/currentLocationUtil.ts';
 import { useRandomRooms } from '../hooks/room.tsx';
+import banner1 from '../assets/images/banner1.jpg';
+import banner2 from '../assets/images/banner2.jpg';
 
 const SpinnerContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+`;
+
+const AddContainer = styled.div`
+  width: 100%;
+  height: 30vh;
+  border-radius: 20px;
+  margin-bottom: 2vh;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface AddImageProps {
+  show: boolean;
+}
+
+const AddImage = styled.img.attrs<AddImageProps>(({ show }) => ({
+  style: {
+    opacity: show ? 1 : 0,
+  },
+}))<AddImageProps>`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  transition: opacity 1s ease-in-out;
+`;
+
+const MainTitle = styled.div`
+  height: 7vh;
+  margin-top: 4vh;
+  font-size: 4vh;
+  font-weight: 500;
+  color: #818080;
+  text-align: center;
+`;
+
+const MainFooter = styled.div`
+  height: 20vh;
+  margin-top: 2vh;
+  border-top: 1px solid #dcdcdc;
+  padding: 2vh 5vh;
+`;
+
+const FooterLogo = styled.div`
+  margin-bottom: 2vh;
+  font-size: 4vh;
+  font-weight: bold;
+  color: #999999;
+`;
+
+const Copywriter = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const FooterContent = styled.p`
+  font-size: 1.5vh;
+  line-height: 2vh;
+`;
+
+const MembersCotainer = styled.p`
+  display: flex;
+  text-align: center;
+`;
+
+const MemberTeam = styled.div`
+  font-size: 3vh;
+  font-weight: bold;
+  margin-bottom: 2vh;
+  color: #999999;
+`;
+
+const FeMembers = styled.div`
+  margin-right: 4vh;
+  flex-direction: column;
+`;
+
+const BeMembers = styled.div`
+  flex-direction: column;
+`;
+
+const Members = styled.p`
+  font-size: 2vh;
+  line-height: 3vh;
+  color: #999999;
+  text-align: center;
 `;
 
 const rotate = keyframes`
@@ -32,6 +128,19 @@ const Spinner = styled.div`
 
 function MainPage() {
   const location = useGeolocation();
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const images = [
+    { id: 'banner1', src: banner1 },
+    { id: 'banner2', src: banner2 },
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    }, 4000);
+    return () => clearInterval(interval);
+  }, [images.length]);
 
   const lat = useMemo(() => location.coordinates!.lat, [location.coordinates]);
   const lng = useMemo(() => location.coordinates!.lng, [location.coordinates]);
@@ -55,7 +164,6 @@ function MainPage() {
     );
   }
 
-  // 위치 정보를 확인하는 중일 때는 스피너를 표시
   if (isLoading) {
     return (
       <SpinnerContainer>
@@ -72,13 +180,59 @@ function MainPage() {
     );
   }
 
-  // 위치 정보가 확인되었을 때는 리스트를 렌더링
   return data ? (
-    <CardGrid listings={data as RoomResponse[]} />
+    <>
+      <AddContainer>
+        {images.map((image) => (
+          <AddImage
+            key={image.id}
+            src={image.src}
+            show={image.id === images[currentImage].id}
+          />
+        ))}
+      </AddContainer>
+      <MainTitle>주변 숙소</MainTitle>
+      <CardGrid listings={data as RoomResponse[]} />
+      <MainFooter>
+        <ContentContainer>
+          <Copywriter>
+            <FooterLogo>aircnc</FooterLogo>
+            <FooterContent>
+              (주)aircnc |대표이사 : 변희준 | 사업자 등록번호 : 22X-8X-4XXX5 |
+              통신판매업신고 : 강남-142XX호 | 메일 : plz@aircnc.com
+            </FooterContent>
+            <FooterContent>
+              관광사업자 등록번호 : 제2024-3x호 | 주소 : 평양시 강남구 장벽2동
+              함흥냉면 2층 3번테이블 옆자리
+            </FooterContent>
+            <FooterContent>
+              호스팅서비스 제공자 : 주식회사 에어씨엔시 고객센터 : 16xx-1xxx
+              (오전 9시 - 오후 00시)
+            </FooterContent>
+          </Copywriter>
+          <MembersCotainer>
+            <FeMembers>
+              <MemberTeam>Front-End</MemberTeam>
+              <Members>변희준</Members>
+              <Members>김상화</Members>
+              <Members>박성현</Members>
+            </FeMembers>
+            <BeMembers>
+              <MemberTeam>Back-End</MemberTeam>
+              <Members>송민석</Members>
+              <Members>길보미</Members>
+              <Members>안지수</Members>
+              <Members>노성빈</Members>
+            </BeMembers>
+          </MembersCotainer>
+        </ContentContainer>
+      </MainFooter>
+    </>
   ) : (
-    <div>
+    <>
       <p>주위에 예약 가능한 숙소가 없습니다.</p>
-    </div>
+      <MainFooter />
+    </>
   );
 }
 
